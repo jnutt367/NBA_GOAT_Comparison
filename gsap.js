cc351f206ccf1b79840e6e677ba2179579c933a8
Jason Nutt <jnutt367@gmail.com>
2021-09-01 11:06:14 -0400

    added image anaimated

--- gsap.js
+++ gsap.js
@@ -2,4 +2,5 @@ gsap.fromTo("h1", { x: -1000 }, { duration: 6, x: 0 });
 gsap.to("p", { y: -400, duration: 4 });
 gsap.to("iframe", { y: -1200 });
 gsap.to(".lead-h3", { duration: 7, y: -500 });
-gsap.to(".following-h3", { duration: 5, y: -500 })
+gsap.to(".following-h3", { duration: 5, y: -500 });
+gsap.fromTo("img", { x: -1400 }, { duration: 1, x: 0 }, 5)
