4debf9c57df39e55bac7bb65886ceb7d95dfecaf
Jason Nutt <jnutt367@gmail.com>
2021-09-01 11:01:57 -0400

    added some gsap3 animations to the ui

---
+++ gsap.js
@@ -0,0 +1,5 @@
+gsap.fromTo("h1", { x: -1000 }, { duration: 6, x: 0 });
+gsap.to("p", { y: -400, duration: 4 });
+gsap.to("iframe", { y: -1200 });
+gsap.to(".lead-h3", { duration: 7, y: -500 });
+gsap.to(".following-h3", { duration: 5, y: -500 })
